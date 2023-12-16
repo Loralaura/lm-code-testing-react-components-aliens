022a2aa6d15ddd7cccc9f43635b3af096c072e93
Laura Robinson <laura3918@hotmail.co.uk>
2023-12-16 23:58:31 +0000

    feat: added SpeciesName component

--- src/components/W12MForm.tsx
+++ src/components/W12MForm.tsx
@@ -1,13 +1,22 @@
-import { useState } from 'react';
-import W12MHeader from './W12MHeader';
+import { useState } from "react";
+import W12MHeader from "./W12MHeader";
+import { SpeciesName } from "./species_name"; //str
+// import { PlanetName } from "./planet_name"; //str
+//import { BeingsNumber } from "./beings_number"; //number?
+//import { MathCheck  } from "./math_check"; //number?
+// import { SparingReason } from "./sparing_reason"; //str
 
 const W12MForm = () => {
-	return (
-		<section className='w12MForm'>
-			<W12MHeader />
-			{/* REST OF FORM GOES HERE */}
-		</section>
-	);
+  const [speciesName, setSpeciesName] = useState("");
+  return (
+    <section className="w12MForm">
+      <W12MHeader />
+      <SpeciesName
+        speciesName={speciesName}
+        onChangeSpeciesName={(value) => setSpeciesName(value)}
+      />
+    </section>
+  );
 };
 
 export default W12MForm;
