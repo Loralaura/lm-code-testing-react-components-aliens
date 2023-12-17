506a5fcb99e75a36174a0202a38a6fe01c12692a
Laura Robinson <laura3918@hotmail.co.uk>
2023-12-17 04:23:06 +0000

    feat: Added more tests to species_name

--- src/components/species_name.test.tsx
+++ src/components/species_name.test.tsx
@@ -11,3 +11,23 @@ test("renders label for species name", () => {
   const labelText = screen.getByText(/Species Name/i);
   expect(labelText).toBeInTheDocument();
 });
+
+test("Species name input field exists", async () => {
+  const requiredProps = {
+    speciesName: "",
+    onChangeSpeciesName: () => {},
+  };
+  render(<SpeciesName {...requiredProps} />);
+  const inputField = screen.getByLabelText("Species Name");
+  expect(inputField).toBeInTheDocument();
+});
+
+test("Species name input field displays value passed in through props", async () => {
+  const requiredProps = {
+    speciesName: "Dog",
+    onChangeSpeciesName: () => {},
+  };
+  render(<SpeciesName {...requiredProps} />);
+  const inputField: HTMLInputElement = screen.getByLabelText("Species Name");
+  expect(inputField.value).toBe("Dog");
+});
