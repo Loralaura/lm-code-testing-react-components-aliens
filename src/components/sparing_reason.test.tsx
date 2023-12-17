f3142ee40ec6be3177ab2c237fb9f81d2b6e97b4
Laura Robinson <laura3918@hotmail.co.uk>
2023-12-17 04:23:25 +0000

    feat: Added more tests for sparing_reason

--- src/components/sparing_reason.test.tsx
+++ src/components/sparing_reason.test.tsx
@@ -11,3 +11,24 @@ test("renders label for Sparing Reason", () => {
   const labelText = screen.getByText(/Reason for Sparing/i);
   expect(labelText).toBeInTheDocument();
 });
+
+test("Sparing Reason input field exists", async () => {
+  const requiredProps = {
+    sparingReason: "",
+    onChangeSparingReason: () => {},
+  };
+  render(<SparingReason {...requiredProps} />);
+  const inputField = screen.getByLabelText(/Reason for Sparing/i);
+  expect(inputField).toBeInTheDocument();
+});
+
+test("Sparing Reason input field displays value passed in through props", async () => {
+  const requiredProps = {
+    sparingReason: "lol",
+    onChangeSparingReason: () => {},
+  };
+  render(<SparingReason {...requiredProps} />);
+  const inputField: HTMLInputElement =
+    screen.getByLabelText(/Reason for Sparing/i);
+  expect(inputField.value).toBe("lol");
+});
