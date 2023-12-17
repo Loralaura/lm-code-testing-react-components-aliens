6ea720491306b1562e9a1835bd6b42784263e208
Laura Robinson <laura3918@hotmail.co.uk>
2023-12-17 05:33:00 +0000

    feat: Added more tests for planet_name

--- src/components/planet_name.test.tsx
+++ src/components/planet_name.test.tsx
@@ -11,3 +11,23 @@ test("renders label for Planet Name", () => {
   const labelText = screen.getByText(/Planet Name/i);
   expect(labelText).toBeInTheDocument();
 });
+
+test("Planet Name input field exists", async () => {
+  const requiredProps = {
+    planetName: "",
+    onChangePlanetName: () => {},
+  };
+  render(<PlanetName {...requiredProps} />);
+  const inputField = screen.getByLabelText(/Planet Name/i);
+  expect(inputField).toBeInTheDocument();
+});
+
+test("Planet Name input field displays value passed in through props", async () => {
+  const requiredProps = {
+    planetName: "Pluto",
+    onChangePlanetName: () => {},
+  };
+  render(<PlanetName {...requiredProps} />);
+  const inputField: HTMLInputElement = screen.getByLabelText(/Planet Name/i);
+  expect(inputField.value).toBe("Pluto");
+});
